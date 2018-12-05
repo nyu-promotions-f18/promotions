0deba22a7290c238f21fc66293fe8703d2a5f5cf
Yang Gao <yg1751@nyu.edu>
2018-12-05 16:35:09 -0500

    fix https issue & add timing function to step implementation

--- features/steps/promotion_steps.py
+++ features/steps/promotion_steps.py
@@ -8,14 +8,12 @@ import requests
 from behave import *
 from compare import expect, ensure
 from selenium.webdriver.support.ui import Select
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-WAIT_SECONDS = 30
-BASE_URL = getenv('BASE_URL', 'http://localhost:5000/')
-
+WAIT_SECONDS = 10
 
 @given('the following promotions')
 def step_impl(context):
@@ -82,8 +80,13 @@ def step_impl(context, button):
 
 @then('I should see "{name}" in the results')
 def step_impl(context, name):
-    element = context.driver.find_element_by_id('search_results')
-    expect(element.text).to_contain(name)
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        EC.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    expect(found).to_be(True)
 
 
 @then('I should not see "{name}" in the results')
@@ -95,8 +98,13 @@ def step_impl(context, name):
 
 @then('I should see the message "{message}"')
 def step_impl(context, message):
-    element = context.driver.find_element_by_id('flash_message')
-    expect(element.text).to_contain(message)
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        EC.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    expect(found).to_be(True)
 
 @then('There should be "{count}" promotions')
 def step_impl(context, count):
@@ -116,8 +124,13 @@ def step_impl(context, count):
 @then('I should see "{text_string}" in the "{element_name}" field')
 def step_impl(context, text_string, element_name):
     element_id = 'promo_' + element_name.lower()
-    element = context.driver.find_element_by_id(element_id)
-    expect(element.get_attribute('value')).to_equal(text_string)
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        EC.text_to_be_present_in_element_value(
+            (By.ID, element_id),
+            text_string
+        )
+    )
+    expect(found).to_be(True)
 
 @when('I change "{element_name}" to "{text_string}"')
 def step_impl(context, element_name, text_string):
