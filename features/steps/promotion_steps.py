e3008653e71b34b187677c2b2780ddb465388a5d
Yang Gao <yg1751@nyu.edu>
2018-12-12 16:44:28 -0500

    extend wait time

--- features/steps/promotion_steps.py
+++ features/steps/promotion_steps.py
@@ -13,7 +13,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-WAIT_SECONDS = 30
+WAIT_SECONDS = 120
 
 @given('the following promotions')
 def step_impl(context):
